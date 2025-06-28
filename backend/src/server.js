b6c9874a2ad32fb0bac30ecf6a27ef70b0c93315
Gabriel <ga1312co-s@student.lu.se>
2025-06-28 11:17:09 +0200

    Deployment fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,6 +37,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
